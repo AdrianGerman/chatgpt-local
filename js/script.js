c817ddbea84ef5b7d63200f05125a85034d8a52b
AdrianGerman <adrian.german1019@gmail.com>
2024-10-02 07:16:22 -0600

    format code, model url and styles

--- js/script.js
+++ js/script.js
@@ -1,95 +1,95 @@
-import { CreateWebWorkerMLCEngine } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm";
+import { CreateWebWorkerMLCEngine } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
 
-const $ = (el) => document.querySelector(el);
+const $ = (el) => document.querySelector(el)
 
-const $form = $("form");
-const $input = $("input");
-const $template = $("#message-template");
-const $messages = $("ul");
-const $container = $("main");
-const $button = $("button");
-const $info = $("small");
-const $loading = $(".loading");
+const $form = $("form")
+const $input = $("input")
+const $template = $("#message-template")
+const $messages = $("ul")
+const $container = $("main")
+const $button = $("button")
+const $info = $("small")
+const $loading = $(".loading")
 
-let messages = [];
-let end = false;
+let messages = []
+let end = false
 
 // este es el modelo que usa de AI, puedes cambiarlo.
-const SELECTED_MODEL = "gemma-2b-it-q4f32_1-MLC";
+const SELECTED_MODEL = "gemma-2b-it-q4f32_1-MLC-1k"
 
 const engine = await CreateWebWorkerMLCEngine(
   new Worker("../js/worker.js", { type: "module" }),
   SELECTED_MODEL,
   {
     initProgressCallback: (info) => {
-      $info.textContent = info.text;
+      $info.textContent = info.text
       if (info.progress === 1 && !end) {
-        end = true;
-        $loading?.parentNode?.removeChild($loading);
-        $button.removeAttribute("disabled");
+        end = true
+        $loading?.parentNode?.removeChild($loading)
+        $button.removeAttribute("disabled")
         addMessage(
           "¡Hola! Soy un ChatGPT que se ejecuta completamente en tu navegador. ¿En qué puedo ayudarte hoy?",
-          "bot",
-        );
-        $input.focus();
+          "bot"
+        )
+        $input.focus()
       }
-    },
-  },
-);
+    }
+  }
+)
 
 $form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const messageText = $input.value.trim();
+  event.preventDefault()
+  const messageText = $input.value.trim()
 
   if (messageText !== "") {
-    $input.value = "";
+    $input.value = ""
   }
-  addMessage(messageText, "user");
-  $button.setAttribute("disabled", "");
+  addMessage(messageText, "user")
+  $button.setAttribute("disabled", "")
 
   const userMessage = {
     role: "user",
-    content: messageText,
-  };
+    content: messageText
+  }
 
-  messages.push(userMessage);
+  messages.push(userMessage)
 
   const chunks = await engine.chat.completions.create({
     messages,
-    stream: true,
-  });
+    stream: true
+  })
 
-  let reply = "";
+  let reply = ""
 
-  const $botMessage = addMessage("", "bot");
+  const $botMessage = addMessage("", "bot")
 
   for await (const chunk of chunks) {
-    const [choice] = chunk.choices;
-    const content = choice?.delta?.content ?? "";
-    reply += content;
-    $botMessage.textContent = reply;
-    $container.scrollTop = $container.scrollHeight;
+    const [choice] = chunk.choices
+    const content = choice?.delta?.content ?? ""
+    reply += content
+    $botMessage.textContent = reply
+    $container.scrollTop = $container.scrollHeight
   }
 
-  $button.removeAttribute("disabled");
-  messages.push({ role: "assistant", content: reply });
-  $container.scrollTop = $container.scrollHeight;
-});
+  $button.removeAttribute("disabled")
+  messages.push({ role: "assistant", content: reply })
+  $container.scrollTop = $container.scrollHeight
+})
 
 function addMessage(text, sender) {
   // clonar el template
-  const clonedTemplate = $template.content.cloneNode(true);
-  const $newMessage = clonedTemplate.querySelector(".message");
+  const clonedTemplate = $template.content.cloneNode(true)
+  const $newMessage = clonedTemplate.querySelector(".message")
 
-  const $who = $newMessage.querySelector("span");
-  const $text = $newMessage.querySelector("p");
+  const $who = $newMessage.querySelector("span")
+  const $text = $newMessage.querySelector("p")
 
-  $text.textContent = text;
-  $who.textContent = sender === "bot" ? "GPT" : "Tú";
-  $newMessage.classList.add(sender);
+  $text.textContent = text
+  $who.textContent = sender === "bot" ? "GPT" : "Tú"
+  $newMessage.classList.add(sender)
 
-  $messages.appendChild($newMessage);
-  $container.scrollTop = $container.scrollHeight;
+  $messages.appendChild($newMessage)
+  $container.scrollTop = $container.scrollHeight
 
-  return $text;
+  return $text
 }
--- js/worker.js
+++ js/worker.js
@@ -1,7 +1,7 @@
-import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm";
+import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
 
-const handler = new WebWorkerMLCEngineHandler();
+const handler = new WebWorkerMLCEngineHandler()
 
 self.onmessage = (msg) => {
-  handler.onmessage(msg);
-};
+  handler.onmessage(msg)
+}
